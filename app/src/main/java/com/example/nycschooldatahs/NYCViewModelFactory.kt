9d298b414cc8aacff4dd934359c3fd4f1398250d
harshals25 <35043807+harshals25@users.noreply.github.com>
2024-04-10 09:51:50 -0400

    Update NYCViewModelFactory.kt

--- app/src/main/java/com/example/nycschooldatahs/NYCViewModelFactory.kt
+++ app/src/main/java/com/example/nycschooldatahs/NYCViewModelFactory.kt
@@ -7,6 +7,7 @@ class NYCViewModelFactory constructor(private val repository: NYCRepository, pri
 
     // helps with separation of concerns if we have more viewModels
     // also good practice to do so for bigger projects as it enables dependency injection as well
+    // really not sure if this is needed tho, maybe there is a better way to define architecture for this project which wont require this class?!
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return if (modelClass.isAssignableFrom(NYCViewModel::class.java)) {
             NYCViewModel(this.repository, header) as T
